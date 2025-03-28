98a24031d45df813be93af04b41752d0512c0ace
Nahngom Zyegan Naam <nahngompansha@gmail.com>
2025-03-28 23:43:22 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,3 @@
-// import { scene, camera, renderer, controls } from "./Scene_Setup.js";
-// import { loadModel } from "./Model_Loader.js";
-// import { setupInteractions } from "./Interaction_Handler.js";
-// import { composer, animate } from "./Post_Processing.js";
-// import { moveCameraToObject, navigateToObject } from "./Room_Navigation.js";
-
-// document.body.appendChild(renderer.domElement);
-
-// loadModel(scene);
-// setupInteractions(scene, camera);
-// animate();
-
 import { SceneSetup } from "./Scene_Setup.js";
 import { ModelLoader } from "./Model_Loader.js";
 import { PostProcessing } from "./Post_Processing.js";
