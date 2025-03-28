622e0e5c935dc8fd3bc5538077a38430ec83263b
Nahngom Zyegan Naam <nahngompansha@gmail.com>
2025-03-28 23:41:09 +0530

    Update Post_Processing.js

--- JS/Post_Processing.js
+++ JS/Post_Processing.js
@@ -1,35 +1,5 @@
-    // import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.131/build/three.module.js';
-    // import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/EffectComposer.js';
-    // import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/RenderPass.js';
-    // import { OutlinePass } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/OutlinePass.js';
-    // import { scene, camera, renderer, controls } from './Scene_Setup.js';
 
-    // const width = window.innerWidth;
-    // const height = window.innerHeight;
-
-    // const composer = new EffectComposer(renderer);
-    // const renderPass = new RenderPass(scene, camera);
-    // composer.addPass(renderPass);
-
-    // const outlinePass = new OutlinePass(new THREE.Vector2(width, height), scene, camera);
-    // outlinePass.edgeStrength = 4;
-    // outlinePass.edgeGlow = 1;
-    // outlinePass.edgeThickness = 3;
-    // outlinePass.pulsePeriod = 0;
-    // outlinePass.visibleEdgeColor.set('#0000ff');
-    // outlinePass.hiddenEdgeColor.set('#0000ff');
-    // composer.addPass(outlinePass);
-
-    // function animate() {
-    //     requestAnimationFrame(animate);
-    //     composer.render();
-    //     controls.update();
-    // }
-
-    // export { composer, outlinePass, animate };
-
-
-    import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.131/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.131/build/three.module.js';
 import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/RenderPass.js';
 import { OutlinePass } from 'https://cdn.jsdelivr.net/npm/three@0.131/examples/jsm/postprocessing/OutlinePass.js';
