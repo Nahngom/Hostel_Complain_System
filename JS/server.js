2d234bd3267df52c0b0f3a4b86ad93986094f5ef
Nahngom Zyegan Naam <nahngompansha@gmail.com>
2025-03-28 23:52:51 +0530

    Update server.js

--- JS/server.js
+++ JS/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'zyegan@14',
+    password: 'enter mysql password :)',
     database: 'object_reports'
 });
 
